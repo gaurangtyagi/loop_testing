b32a59b5e96b3cfbb441bceecf40192d31bdaf7f
Gaurang Tyagi <85627251+gaurangtyagi@users.noreply.github.com>
2021-06-20 00:17:24 +0530

    Function added

--- triangle.cpp
+++ triangle.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
     using namespace std;
-
+int triangle(int a1)
+{
+    for (int i = 1; i <=a1; i++)
+        {
+            cout << "\n";
+            for (int j = a1; j>=i; j--)
+            {
+            cout <<"* ";
+            }
+        }
+}
     int main ()
     {
         int a; 
         cout << "Enter the length of base of triangle ";
         cin >> a;
-
-        for (int i = 1; i <=a; i++)
-        {
-            cout << "\n";
-            for (int j = a; j>=i; j--)
-            {
-            cout <<"* ";
-            }
-        }
+        
+        triangle(a);
+        
         return 0;
     }
